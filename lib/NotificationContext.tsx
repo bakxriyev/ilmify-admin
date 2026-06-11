a99a5ca7496f6bef1bb3f1c622a2ff00e603c43c
bakxriyev <kamronbekbahriyev18@gmail.com>
2026-06-11 16:17:17 +0500

    dsdsdsd

--- lib/NotificationContext.tsx
+++ lib/NotificationContext.tsx
@@ -99,6 +99,6 @@ export function NotificationProvider({ children }: { children: ReactNode }) {
 
 export function useNotifications() {
   const ctx = useContext(NotificationContext);
-  if (!ctx) throw new Error('useNotifications must be inside NotificationProvider');
+  if (!ctx) throw new Error('useNotifications must be inside NotificationProviderss');
   return ctx;
 }
